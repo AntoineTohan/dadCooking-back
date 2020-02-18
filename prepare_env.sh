0f61b4bb472063cda65e821273301ecdd8ad9057
AntoineTohan <antoine.durand2@epsi.fr>
2020-02-18 13:39:17 +0100

    update script prepare env for create an light database

--- prepare_env.sh
+++ prepare_env.sh
@@ -1,3 +1,4 @@
 # pull iimage sql and prepare database dadcooking
 docker pull mysql:5.7
 docker run -p 3306:3306 --name dadcooking-db -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=dadcooking -d mysql:5.7
+cat backup.sql | docker exec -i dadcooking-db /usr/bin/mysql -u root --password=password dadcooking
